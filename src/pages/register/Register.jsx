44f61bba7cba7a9cf0a1d7435255f2c2c7f9bc75
Fabian Forero <fabian.400@outlook.com>
2022-06-03 21:13:22 -0500

    Agregando Vista de Registro

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,28 @@
+import './register.css';
+
+const Register = () => {
+  return (
+    <div className="login">
+    <div className="loginWrapper">
+      <div className="loginLeft">
+        <h3 className="loginLogo">Lamasocial</h3>
+        <span className="loginDesc">
+          Connect with friends and the world around you on Lamasocial.
+        </span>
+      </div>
+      <div className="loginRight">
+        <div className="loginBox">
+          <input type="text" placeholder='Username' className="loginInput" />
+          <input type="email" placeholder='Email' className="loginInput" />
+          <input type="password" placeholder='Password' className="loginInput" />
+          <input type="password" placeholder='Password Again' className="loginInput" />
+          <button className="loginButton">Sing Up</button>
+          <button className="loginRegisterButton">Log into Account</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Register;
